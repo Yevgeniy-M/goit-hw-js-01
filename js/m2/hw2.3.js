fa116ea8a0582b8f383fea24de0e812ca5c41f89
Yevgeniy-M <67243679+Yevgeniy-M@users.noreply.github.com>
2020-10-15 01:33:15 +0300

    hw 2.3

--- js/m2/hw2.3.js
+++ js/m2/hw2.3.js
@@ -1,12 +1,19 @@
-function findLongestWord=(string = "") {
-    return string.split(' ').length;    
-  };
-  
-  console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
-  // 'jumped'
-  
-  console.log(findLongestWord('Google do a roll'));
-  // 'Google'
-  
-  console.log(findLongestWord('May the force be with you'));
-  // 'force'
+function findLongestWord (string = "") {
+  let word = string.split(' ');
+  let longestWord = '';
+  for (let i = 0; i < word.length; i += 1) {
+  	if (word[i].length > longestWord.length) {
+    	longestWord = word[i];
+    	}
+  	}
+  return longestWord;
+}
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// 'jumped'
+
+console.log(findLongestWord('Google do a roll'));
+// 'Google'
+
+console.log(findLongestWord('May the force be with you'));
+// 'force'
