a18b233dab09f0e22830b3d6e7be24e1ca44abbb
Yevgeniy-M <jhorg.htk@gmail.com>
2020-10-15 17:03:20 +0300

    Create hw2.5

---
+++ js/m2/hw2.5.js
@@ -0,0 +1,13 @@
+function checkForSpam (str) { 
+  'use strict';
+  // Write code under this line
+  
+}
+
+//console.log(checkForSpam('Latest technology news')); // false
+
+//console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+//console.log(checkForSpam('Get best sale offers now!')); // true
+
+//console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
