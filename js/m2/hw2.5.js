347ea5196b8a8259ecfc815f1996b22467a1fe2e
Yevgeniy-M <jhorg.htk@gmail.com>
2020-10-16 17:00:25 +0300

    основа hw2.5

--- js/m2/hw2.5.js
+++ js/m2/hw2.5.js
@@ -1,13 +1,18 @@
-function checkForSpam (str) { 
+const checkForSpam = function (str) {
   'use strict';
-  // Write code under this line
+  // return str.includes('spam' || 'sale' || 'spam'.toUpperCase() || 'sale'.toUpperCase()));
+  // return str.includes(('spam' || 'spam'.toUpperCase()) || str.includes('sale' || 'sale'.toUpperCase()));
   
+  // return str.includes(('spam'.toUpperCase()) || str.includes('sale'.toUpperCase()));
+
+  return str.toUppercase(str.includes('sale' || 'SPAM'));
+
 }
 
-//console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('Latest technology news')); // false
 
-//console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
 
-//console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Get best sale offers now!')); // true
 
-//console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
